17a8b083d0a00dea601c831aebe156b223db1c3c
exena <exena@naver.com>
2025-01-12 17:15:59 +0900

    Initial commit

---
+++ src/main/java/com/anthive/blogservice/BlogserviceApplication.java
@@ -0,0 +1,13 @@
+package com.anthive.blogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogserviceApplication.class, args);
+	}
+
+}
