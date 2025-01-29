bb3fe2140318347c1dcfeded401ff3d75068509d
exena <exena@naver.com>
2025-01-29 18:52:00 +0900

    feat: Add sidebar (frontend)

--- src/main/java/com/anthive/blogservice/Hello.java
+++ src/main/java/com/anthive/blogservice/Hello.java
@@ -1,14 +1,14 @@
 package com.anthive.blogservice;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Hello {
 
     @GetMapping("/")
     public String index()
     {
-        return "Hello Spring Boot!";
+        return "index";
     }
 }
